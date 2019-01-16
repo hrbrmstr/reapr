5f29f5198cbb093653e16972726c472d0a63ac20
hrbrmstr <bob@rud.is>
2019-01-15 19:34:04 -0500

    R package repo initialization complete

---
+++ R/reapr-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/reapr>
+#' - BugReports: <https://gitlab.com/hrbrmstr/reapr/issues>
+#' 
+#' @md
+#' @name reapr
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
